095b158f782ca451278de6168b75d2b6d69ff8c4
Sooparkle <imsoohanmu@gmail.com>
2024-07-19 17:46:33 +0900

    Design(Header) : Modify header UI + Style(Header) : Revised some codes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,65 +1,30 @@
-'use client'
-
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import styles from '../styles/Header.module.scss';
-import { usePathname, useRouter } from 'next/navigation';
-import { CiMountain1, CiUser, CiMenuBurger } from "react-icons/ci";
 import SearchForm from './SearchForm';
 import NotSearchForm from './NotSearchForm';
+import NavList from './NavList';
+
 
 interface HeaderProps{
   type:string
 }
 
+
 export const Header = ({type}:HeaderProps)  => {
 
-  console.log("type", type)
-  const pathname = usePathname();
-  const isMypageIncluded = pathname.includes('/mypage');
-  
   return (
     <header
       className={styles.header}
     >
-      <nav
-        className={styles.nav}
-      >
-        <ul >
-          <li
-            className={`${pathname === "/" ? styles.active : ""}`}
-          >
-            <Link
-            href="/"
-            
-          >
-            <CiMountain1
-            className={styles.main}/>
-          </Link>
-          </li>
-
-          <li
-            className={`${isMypageIncluded ? styles.active : ""}`}
-          >
-            <Link
-            href="/mypage"
-            >
-            <CiUser 
-            className={styles.profile}
-            />
-          </Link>
-          </li>
-        </ul>
-
-      </nav>
+      <NavList />
 
       {
         type === "mypage" && <NotSearchForm />
       }
-
       
-      {type === 'main' &&  <SearchForm />}
+      {
+        type === 'main' &&  <SearchForm />
+      }
       
 
     </header>
---
+++ app/components/NavList.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import React, { useState } from 'react'
+import Link from 'next/link'
+import styles from '../styles/Header.module.scss';
+import { usePathname } from 'next/navigation'
+import  { IoPersonOutline } from "react-icons/io5";
+import { CiMountain1, CiMenuBurger,CiCircleList } from "react-icons/ci";
+import Image from 'next/image';
+
+const MyinfoList = () =>{
+
+  const loginInfo = false
+  return(
+    <section
+      className={styles.headerPopupWrap}
+    >
+      <div
+        className={styles.popupImage}
+      >
+        {
+          loginInfo ? (
+            <Link href='/maypge' >
+              <Image 
+              src='https://images.unsplash.com/photo-1599687351724-dfa3c4ff81b1?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              fill
+              alt='본인 아바타'
+              
+              />
+            </Link>
+          ) : (
+            <Link href='/login'>
+              <IoPersonOutline />
+            </Link>
+          )
+        }
+        <p>
+          {
+            loginInfo ? "홍길동님" :"로그인 필요"
+          }
+        </p>
+      </div>
+
+      <ul>
+        <li>
+          <Link href='/notice'>공지사항</Link>
+        </li>
+        <li>
+          <Link href='/event'>이벤트</Link>
+        </li>
+        <li>
+          <Link href='/faq'>FAQ</Link>
+        </li>
+      </ul>
+    </section >
+  )
+}
+
+const NavList = () => {
+  const [ mypageClicked, setMypagClicked ] = useState<boolean>(false)
+
+  const pathname = usePathname();
+  const isMypageIncluded = pathname.includes('/mypage');
+  
+  return (
+    <nav
+    className={styles.nav}
+  >
+    <ul >
+      <li
+        className={`${pathname === "/" ? styles.active : ""}`}
+      >
+        <Link
+        href="/"
+        
+      >
+        <CiMountain1
+        className={styles.main}/>
+      </Link>
+      </li>
+
+      <li
+        className={`${styles.headerMypageIcon} ${isMypageIncluded ? styles.active : ""}`}
+        onClick={()=>setMypagClicked(true)}
+      >
+        <CiCircleList 
+        className={styles.profile}
+        />{mypageClicked && <MyinfoList />}
+      </li>
+
+    </ul>
+
+  </nav>
+  )
+}
+
+export default NavList
