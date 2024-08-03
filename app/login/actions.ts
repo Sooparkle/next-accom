12263473e430383a5280f353157634891d759cf0
Sooparkle <imsoohanmu@gmail.com>
2024-08-03 21:54:16 +0900

    Feat(Auth) : updated auth middleware through Supabase

---
+++ app/login/actions.ts
@@ -0,0 +1,55 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function emailLogin(formData: FormData) {
+  const supabase = createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/login?message=Failed to authentication')
+  }
+
+  revalidatePath('/', 'layout') // make all cach reset for login user
+  redirect('/')
+}
+
+
+export async function singOut () {
+  const supabase = createClient();
+  await supabase.auth.signOut()
+  redirect('/login');
+}
+
+// singup function
+
+// export async function signup(formData: FormData) {
+//   const supabase = createClient()
+
+//   // type-casting here for convenience
+//   // in practice, you should validate your inputs
+//   const data = {
+//     email: formData.get('email') as string,
+//     password: formData.get('password') as string,
+//   }
+
+//   const { error } = await supabase.auth.signUp(data)
+
+//   if (error) {
+//     redirect('/error')
+//   }
+
+//   revalidatePath('/', 'layout')
+//   redirect('/')
+// }
--- app/util/types.ts
+++ app/util/types.ts
@@ -23,3 +23,47 @@ export interface AccomDataType {
   updated_at: string;
   rooms?:boolean;
 }
+
+
+export interface ConfirmDataType {
+  startDate: Date | null;
+  endDate: Date | null;
+  totalNights: number | null;
+  totalNumbers?: number | null;
+  totalPrice : number;
+  adult?: number | null;
+  child?: number | null;
+}
+
+
+export interface UserType {
+  id: string;
+  aud: string;
+  role: string;
+  email: string;
+  email_confirmed_at: string;
+  phone: string;
+  confirmed_at: string;
+  last_sign_in_at: string;
+  app_metadata: {
+    provider: string;
+    providers: string[];
+  };
+  user_metadata:Record<string, unknown>;
+  identities: Identity[];
+  created_at: string;
+  updated_at: string;
+  is_anonymous: boolean;
+}
+
+interface Identity {
+  identity_id: string;
+  id: string;
+  user_id: string;
+  identity_data: Record<string, unknown>;
+  provider: string;
+  last_sign_in_at: string;
+  created_at: string;
+  updated_at: string;
+  email: string;
+}
