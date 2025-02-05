898bf18a0e332dab063dc838dcc2be64aa5e895b
Sooparkle <imsoohanmu@gmail.com>
2025-02-05 12:05:09 +0900

    Feat(Loading) : Added Loading component

---
+++ app/(Non-Auth)/accommodation/loading.tsx
@@ -0,0 +1,14 @@
+// app/notifications/loading.tsx
+import styles from '@/app/styles/Loading.module.scss'
+
+export default function Loading() {
+
+
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.loadingSpinner} />
+      <h2 className={styles.title}>...로딩 중...</h2>
+      <p className={styles.message}>게시판에 데이터를 가져오고 있습니다.</p>
+    </div>
+  );
+}
